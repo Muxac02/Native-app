90401f9ef8c4ae7c7485d46220ea84c87f7ddbff
Muxac02 <mihail.pilipenko2002@mail.ru>
2025-08-22 21:11:06 +0300

    feat: index page for another task

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import App from "./App";
+import App2 from "./index.tsx";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(App2);
